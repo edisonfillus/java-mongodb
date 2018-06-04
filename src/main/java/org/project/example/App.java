498bed4b20056122ddc39d89a24236fcc187f913
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-06-04 17:47:18 -0300

    Setup Freemarker for HTML Templates

--- src/main/java/org/project/example/App.java
+++ src/main/java/org/project/example/App.java
@@ -1,5 +1,15 @@
 package org.project.example;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
 /**
@@ -7,6 +17,43 @@ import static spark.Spark.*;
  */
 public class App {
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello World from Spark Framework");
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
+        cfg.setClassForTemplateLoading(App.class,"/");
+
+        // Set the preferred charset template files are stored in. UTF-8 is
+        // a good choice in most applications:
+        cfg.setDefaultEncoding("UTF-8");
+
+        // Sets how errors will appear.
+        // During web page *development* TemplateExceptionHandler.HTML_DEBUG_HANDLER is better.
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        // Don't log exceptions inside FreeMarker that it will thrown at you anyway:
+        cfg.setLogTemplateExceptions(false);
+
+        // Wrap unchecked exceptions thrown during template processing into TemplateException-s.
+        cfg.setWrapUncheckedExceptions(true);
+
+
+        get("/", (req, res) -> {
+            Map<String, Object> helloMap = new HashMap<>();
+            helloMap.put("name", "Freemarker");
+
+            StringWriter writer = new StringWriter();
+            try {
+                Template helloTemplate = cfg.getTemplate("hello.ftl");
+                helloTemplate.process(helloMap,writer);
+            } catch (TemplateException e) {
+                halt(500);
+                e.printStackTrace();
+            } catch (IOException e) {
+                halt(500);
+                e.printStackTrace();
+            }
+            return writer;
+        });
+
+
+
     }
 }
